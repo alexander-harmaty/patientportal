75fb49ecdb0cc5da3b2f1820b7223fde7f45489f
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-03-07 15:02:17 -0500

    test

---
+++ src/main/java/com/groupfour/patientportal/Main.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupfour.patientportal;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+/**
+ *
+ * @author Alexander
+ */
+public class Main 
+{
+  public static void main(String[] args) throws   FileNotFoundException 
+  {
+    boolean run = true; 
+    Scanner in = new Scanner(System.in);
+    while(run) 
+    {
+	  System.out.println("Patient Portal UI");
+          System.out.println("------------------");
+	  System.out.println("What operation would you like to do?");
+	  System.out.println("\t1) View Personal Information");
+	  System.out.println("\t2) Insert/Update Personal Information");
+	  System.out.println("\t3) Exit UI");
+	  
+	  int ans = in.nextInt();
+	  
+	  switch(ans) {
+	  case(1):
+		  System.out.println("You are viewing 'Something'");     
+     Scanner read = new Scanner(new File("patient.csv"));
+     read.useDelimiter(":");
+      //String inputFileName = read.nextLine();
+      //FileReader fr = new FileReader(patient.csv);
+      //Scanner input = new Scanner(file);
+    
+     while (read.hasNext()) { 
+        System.out.print(read.next());
+       };
+      read.close();
+        break;
+	  case(2):
+		  System.out.println("You can insert stuff eventually");
+	  		break;
+	  case(3):
+		  System.out.println("die");
+	  	  System.exit(0);
+	  	  	break;
+   }
+  }
+ }
+}
