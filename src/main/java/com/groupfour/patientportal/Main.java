ee74f1b24a56a497facca1644c39054f903e7048
Chrisscheer211 <chrisscheer211@gmail.com>
2022-03-08 15:47:27 -0500

    Revert "Build Test 6 - Chris"
    This reverts commit e6a380d9dd881cc7c71882a43b6ee19ec6d26422.

--- src/main/java/com/groupfour/patientportal/Main.java
+++ src/main/java/com/groupfour/patientportal/Main.java
@@ -2,7 +2,6 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * Chris Was Here
  */
 package com.groupfour.patientportal;
 import javax.swing.JOptionPane;
@@ -12,7 +11,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 /**
- * @testor Christopher
+ *
  * @author Alexander
  */
 public class Main 
@@ -26,9 +25,9 @@ public class Main
 	  System.out.println("Patient Portal UI");
           System.out.println("------------------");
 	  System.out.println("What operation would you like to do?");
-	  System.out.println("\t1) View Personal Information (Test)");
-	  System.out.println("\t2) Insert/Update Personal Information (Test)");
-	  System.out.println("\t3) Exit UI (Test)");
+	  System.out.println("\t1) View Personal Information");
+	  System.out.println("\t2) Insert/Update Personal Information");
+	  System.out.println("\t3) Exit UI");
 	  
 	  int ans = in.nextInt();
 	  
